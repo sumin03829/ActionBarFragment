58b776a4abf2b852f56376b782b486bce3d9e08e
sumin_030829 <sumin0829@kako.com>
2020-11-05 20:43:14 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/s2019s38/actionbarfragment/MainActivity.java
@@ -0,0 +1,25 @@
+package kr.hs.emirim.s2019s38.actionbarfragment;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements ActionBar.TabListener {
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+}
